89174973b5d3fd66ac71a2aed09e873ae69d50b4
Rajan <boudelrajan@gmail.com>
2024-11-23 09:57:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+app.get('/',(req,res)=>{
+    res.send("Hello world")
+});
+app.get('/test',(req,res)=>{
+    res.send("Hello world test")
+});
+const PORT = 8080;
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+})
